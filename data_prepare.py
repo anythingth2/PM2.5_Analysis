0e3bc41d307d45d3862fed33d09afc771cf847bb
ChiChaChai <chatchaishaetan@gmail.com>
2019-12-21 16:30:39 +0700

    Add berkeleyearth preparation

--- data_prepare.py
+++ data_prepare.py
@@ -4,7 +4,8 @@ from datetime import datetime
 import pandas as pd
 from pathlib import Path
 from tqdm import tqdm, trange
-
+import ujson
+import datetime
 # %%
 
 
@@ -40,7 +41,48 @@ def create_sensor_csv(sensor_dir, csv_path):
 
 
 # %%
-create_sensor_csv('dataset/tgr_sensor', 'tgr_sensor.csv')
+# create_sensor_csv('dataset/tgr_sensor', 'tgr_sensor.csv')
+# %%
 
+
+def prepare_berkeley_dataset(input_dir, output_dir):
+    input_dir = Path(input_dir)
+    output_dir = Path(output_dir)
+
+    region_df = []
+    sensor_df = []
+
+    for input_path in tqdm(sorted(input_dir.glob('*.txt'))):
+        with open(input_path, 'r') as f:
+            lines = f.read().split('\n')
+            infos = lines[:9]
+            sensor_header = lines[9]
+            sensor_content = lines[10:]
+
+        info_dict = {}
+        for info in infos:
+            info = info.replace('%', '').replace(' ', '')
+            topic, data = info.lower().split(':')
+            info_dict[topic] = data
+        region_df.append((info_dict['city'], info_dict['region'],
+                          info_dict['latitude'], info_dict['longitude'], info_dict['population']))
+
+        for sensor_data in sensor_content[:-1]:
+            sensor_data = sensor_data.split('\t')
+            timestamp_data = [int(v) for v in sensor_data[:4]]
+            timestamp = datetime.datetime(*timestamp_data)
+            pm = float(sensor_data[4])
+            sensor_df.append((info_dict['city'], timestamp, pm))
+
+    region_df = pd.DataFrame(
+        region_df, columns=['city', 'region', 'lat', 'lng', 'population'])
+    sensor_df = pd.DataFrame(sensor_df, columns=['city', 'timestamp', 'pm'])
+
+    region_df.to_csv(str(output_dir / 'region.csv'))
+    sensor_df.to_csv(str(output_dir / 'sensor.csv'))
+
+
+# %%
+prepare_berkeley_dataset('dataset/raw_berkeleyearth', '')
 
 # %%
