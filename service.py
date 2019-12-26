0524c19c808961c86ef2a2a1fc0a606a2b16a762
ChiChaChai <chatchaishaetan@gmail.com>
2019-12-27 01:56:22 +0700

    Add service with tgr dataset

--- service.py
+++ service.py
@@ -4,8 +4,10 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from keras.models import load_model
 from sklearn.externals import joblib
+
 # %%
 tgr_df = pd.read_csv('dataset/tgr_sensor.csv')
+tgr_df['timestamp'] = pd.to_datetime(tgr_df['timestamp'])
 
 ex_region_df = pd.read_csv('dataset/berkeleyearth/region.csv')
 ex_region_df.rename({'Unnamed: 0': 'id'}, axis=1, inplace=True)
@@ -14,6 +16,67 @@ ex_sensor_df.rename({'Unnamed: 0': 'id'}, axis=1, inplace=True)
 ex_sensor_df['timestamp'] = pd.to_datetime(ex_sensor_df['timestamp'])
 ex_sensor_df = ex_sensor_df.set_index('city')
 # %%
+
+
+grid_size = 0.001
+
+origin_lat = 12
+origin_lng = 90
+
+
+def grid_mapper(row):
+    # row = tgr_df.loc[index]
+    lat = row['lat']
+    lng = row['lng']
+    centroid_lat = origin_lat + (((lat - origin_lat) * 1000 //
+                                  (grid_size * 1000)) / 1000) + (grid_size / 2)
+    centroid_lng = origin_lng + (((lng - origin_lng) * 1000 //
+                                  (grid_size * 1000)) / 1000) + (grid_size / 2)
+    return (centroid_lat, centroid_lng)
+
+
+cells = np.array(tgr_df.apply(grid_mapper, axis=1).to_list())
+
+tgr_df['cell_x'] = cells[:, 0]
+tgr_df['cell_y'] = cells[:, 1]
+# %%
+
+
+def hourly_tgr_resample(df):
+
+    resampled = df.set_index('timestamp').resample('H')
+    return resampled['sensor'].mean().reset_index()
+
+
+def daily_tgr_resample(df):
+
+    resampled = df.set_index('timestamp').resample('D')
+    return resampled['sensor'].mean().reset_index()
+
+
+hourly_tgr_df = tgr_df.groupby(['cell_x', 'cell_y']).apply(hourly_tgr_resample)
+daily_tgr_df = tgr_df.groupby(['cell_x', 'cell_y']).apply(daily_tgr_resample)
+# %%
+
+
+def create_tgr_json(cell_df, last_n=None):
+    lat, lng = cell_df.name
+    if last_n is not None:
+        cell_df = cell_df.tail(last_n)
+
+    return {
+        'lat': lat,
+        'lng': lng,
+        'history': cell_df.apply(lambda row: {'pm25': row['sensor'] if row['sensor'] is not pd.NaT else None,
+                                              'timestamp': row['timestamp']}, axis=1)
+    }
+
+
+# %%
+daily_tgr_df.groupby(['cell_x', 'cell_y']).apply(create_tgr_json, 15).to_list()
+# %%
+hourly_tgr_df.groupby(['cell_x', 'cell_y']).apply(
+    create_tgr_json, 24).to_list()
 
 # %%
 
@@ -71,7 +134,8 @@ def create_city_info_dict(city_info,):
 def create_sensor_record(sensor_df, last_n=None):
     if last_n is not None:
         sensor_df = sensor_df.tail(last_n)
-    return sensor_df.apply(lambda row: {'pm25': row['pm'], 'timestamp': row['timestamp']}, axis=1).to_list()
+    return sensor_df.apply(lambda row: {'pm25': row['pm'] if not pd.isna(row['pm']) else None,
+                                        'timestamp': row['timestamp']}, axis=1).to_list()
 
 
 def create_daily_city(city_info):
@@ -91,8 +155,8 @@ def create_hourly_city(city_info):
     return output_dict
 
 
-ex_region_df.apply(create_hourly_city, axis=1).to_list()
 # %%
+ex_region_df.apply(create_hourly_city, axis=1).to_list()
 
 # %%
 ex_region_df.apply(create_daily_city,  axis=1).to_list()
